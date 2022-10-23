0453c35ba31bbae2b5c4bfb641ebaf4100c73664
V-Emanuel <gvictor567@gmail.com>
2022-10-23 16:19:16 -0300

    Parte com seleção de quantidade de cartas finalizada

--- interacao.js
+++ interacao.js
@@ -1,6 +1,16 @@
+let contador;
+const cartas = [];
 let q_cartas = prompt("Quantas cartas você deseja?");
 
 while(q_cartas < 4 || q_cartas > 14 || q_cartas%2 != 0){
-    q_cartas = prompt("Numero de cartas inválido. Digite novamnte");
+    q_cartas = prompt("Numero de cartas inválido. Digite novamente:");
 }
-alert("tudo certo, nada errado");
+const lista_de_cartas = document.querySelector('ul');
+function adiciona_cartas(){
+    for(i = 0; i < q_cartas; i++){
+        const item = '<li><img class = "papagaio" src="./midia/back.png"/></li>';
+        lista_de_cartas.innerHTML = lista_de_cartas.innerHTML + item;
+    }
+    
+}
+adiciona_cartas()
