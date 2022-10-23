e7c3beeaea40afad19eae3e690f09b3be31ebb2a
V-Emanuel <gvictor567@gmail.com>
2022-10-23 16:59:58 -0300

    versão com a virada da carta

--- interacao.js
+++ interacao.js
@@ -1,16 +1,21 @@
 let contador;
-const cartas = [];
-let q_cartas = prompt("Quantas cartas você deseja?");
+
+let q_cartas = prompt("Digite uma quantidade de cartas em 4 e 14: \n" + "Lembre-se que a quantidade deve ser par.");
 
 while(q_cartas < 4 || q_cartas > 14 || q_cartas%2 != 0){
     q_cartas = prompt("Numero de cartas inválido. Digite novamente:");
 }
 const lista_de_cartas = document.querySelector('ul');
+
 function adiciona_cartas(){
     for(i = 0; i < q_cartas; i++){
-        const item = '<li><img class = "papagaio" src="./midia/back.png"/></li>';
-        lista_de_cartas.innerHTML = lista_de_cartas.innerHTML + item;
-    }
-    
+        const cartas = '<li onclick = "virar(this)"><img class = "papagaio" src="./midia/back.png"/></li>';
+        lista_de_cartas.innerHTML += cartas;
+    }   
 }
+
 adiciona_cartas()
+
+function virar(elemento){
+    elemento.classList.toggle("virada");
+}
