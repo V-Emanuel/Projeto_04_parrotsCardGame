3e41194bb10fe928f41c6c67e29e6b63b71574b6
V-Emanuel <gvictor567@gmail.com>
2022-10-23 23:23:43 -0300

    versão com as cartas embaralhadas

--- interacao.js
+++ interacao.js
@@ -1,24 +1,24 @@
-let contador;
-let img_frente = ['<img class = "frente face" src="./midia/explodyparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/explodyparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/bobrossparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/bobrossparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/fiestaparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/fiestaparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/metalparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/metalparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/revertitparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/revertitparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/tripletsparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/tripletsparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/unicornparrot.gif"/>',
-                    '<img class = "frente face" src="./midia/unicornparrot.gif"/>'];
-img_frente.sort(comparador);
+let contador = 0;
+let carta;
+let cartas_embarlhadas = [];
+let img_frente = ['<div class = "frente face" ><img  src="./midia/explodyparrot.gif"/></div>',
+                    '<div class = "frente face" ><img  src="./midia/explodyparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/bobrossparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/bobrossparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/fiestaparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/fiestaparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/metalparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/metalparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/revertitparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/revertitparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/tripletsparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/tripletsparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/unicornparrot.gif"/></div>',
+                    '<div class = "frente face" ><img src="./midia/unicornparrot.gif"/></div>'];
 function comparador() { 
 	return Math.random() - 0.5; 
 }
-
-const papagaio = '<img class = "verso face" src="./midia/back.png"/>';
+const papagaio = '<div class = "verso face" ><img src="./midia/back.png"/></div>';
 let q_cartas = prompt("Digite uma quantidade de cartas em 4 e 14: \n" + "Lembre-se que a quantidade deve ser par.");
 
 
@@ -30,12 +30,15 @@ const lista_de_cartas = document.querySelector('ul');
 
 function adiciona_cartas(){
     for(i = 0; i < q_cartas; i++){
-        const cartas = '<li onclick = "virar(this)">'+ papagaio + img_frente[i] +'</li >';
-        lista_de_cartas.innerHTML += cartas;
-    }   
+        carta = '<li onclick = "virar(this)">' + img_frente[i] + papagaio +'</li >';
+        cartas_embarlhadas.push(carta);
+    }  
+    cartas_embarlhadas.sort(comparador);
+    lista_de_cartas.innerHTML = cartas_embarlhadas;
 }
 adiciona_cartas()
 
 function virar(elemento){
-    elemento.classList.toggle("virada");
+    elemento.classList.add("virada");
+    contador++;
 }
